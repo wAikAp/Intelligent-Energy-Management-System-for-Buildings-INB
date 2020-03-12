3ca2fe415b92c89c6cc461e6ba67d3ed9101ed60
Ricky-HPY <60127532+HPY-ricky@users.noreply.github.com>
2020-03-12 16:13:39 +0800

    change connectionString
    change connectionString
    data display to Devices page

--- FYP_WEB_APP/FYP_WEB_APP/Controllers/DevicesController.cs
+++ FYP_WEB_APP/FYP_WEB_APP/Controllers/DevicesController.cs
@@ -15,27 +15,37 @@ namespace FYP_WEB_APP.Controllers
 		public IActionResult Devices()
 		{
 
-			
-			System.Diagnostics.Debug.WriteLine("Hello..........");
+			/*
+			Debug.WriteLine("Hello..........");
 			Debug.WriteLine("");
 			Debug.WriteLine("");
 			Debug.WriteLine("");
 			Debug.WriteLine("");
 			Debug.WriteLine("");
+			*/
+			var connectionString = ("mongodb://admin:admin@clustertest-shard-00-00-kjhvv.azure.mongodb.net:27017,clustertest-shard-00-01-kjhvv.azure.mongodb.net:27017,clustertest-shard-00-02-kjhvv.azure.mongodb.net:27017/test?ssl=true&replicaSet=ClusterTest-shard-0&authSource=admin&retryWrites=true&w=majority");
 
-			/*var connectionString = "mongodb+srv://admin:admin@clustertest-kjhvv.azure.mongodb.net/test?retryWrites=true&w=majority";
+			//var connectionString = "mongodb+srv://admin:admin@clustertest-kjhvv.azure.mongodb.net/test?retryWrites=true&w=majority";
 			MongoClient dbClient = new MongoClient(connectionString);
 			var database = dbClient.GetDatabase("FYP_1920");
 			var collection = database.GetCollection<MongoLightListModel>("LIGHT_LIST");
 
 			var documents = collection.Find(new BsonDocument()).ToList();
-			foreach(MongoLightListModel ll in documents)
+			var datalist = new List<MongoLightListModel> { };
+
+			foreach (MongoLightListModel ll in documents)
 			{
-				Debug.WriteLine(ll.lastest_checking_time);
-			}*/
+				//Debug.WriteLine(ll.lastest_checking_time);
+				//Debug.WriteLine(ll.roomId);
+				var x = new MongoLightListModel()
+				{
+					roomId = ll.roomId
+				};
+				datalist.Add(x);
+			}
 
 
-			return View();
+			return View(datalist);
 		}
 	}
 }
