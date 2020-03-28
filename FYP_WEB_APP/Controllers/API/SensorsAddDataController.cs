a6db0f30308c5cb0ef97d1d2819c4fd6b2820c32
Ricky-HPY <60127532+HPY-ricky@users.noreply.github.com>
2020-03-28 20:10:19 +0800

    add api - SensorsAddDataController

---
+++ FYP_WEB_APP/Controllers/API/SensorsAddDataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FYP_WEB_APP.Controllers.Mongodb;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace FYP_WEB_APP.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SensorsAddDataController : ControllerBase
+    {
+        public object Post(dynamic SensorJson)
+        {
+            var str = "";
+            ConnectDB conn = new ConnectDB();
+            IMongoDatabase database = conn.Conn();
+
+            var collection = database.GetCollection<BsonDocument>("SENSOR");
+
+            foreach (var S in SensorJson)
+            {
+                string id = S.SensorId;
+                double Value = S.Value;
+                DateTime utcNow = DateTime.UtcNow;
+                str += "{ SensorId , " + id + "},{ Value," + Value + "},{ UserPostDate," + utcNow + "}\n";
+
+                collection.InsertOne(new BsonDocument { { "SensorId", id }, { "Value", Value }, { "UserPostDate", utcNow } });
+
+            }
+            return "data==" + str;
+        }
+    }
+}
