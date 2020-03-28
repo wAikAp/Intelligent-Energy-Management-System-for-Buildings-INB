8d980e8400ce72d7c664167725fc9ff6979ee5f1
Ricky-HPY <60127532+HPY-ricky@users.noreply.github.com>
2020-03-28 20:28:13 +0800

    create room list model

---
+++ FYP_WEB_APP/Models/RoomsListModel.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP_WEB_APP.Models
+{
+    public class RoomsListModel
+    {
+        public ObjectId _id { get; set; }
+        public string roomId { get; set; }
+        public string desc { get; set; }
+
+        public string temp { get; set; }
+        public string hum { get; set; }
+        public string lig { get; set; }
+        public string lightListId { get; set; }
+        public string acListId { get; set; }
+        public string humListId { get; set; }
+        public string exhasFanListId { get; set; }
+        public string sensorListId { get; set; }
+    }
+}
