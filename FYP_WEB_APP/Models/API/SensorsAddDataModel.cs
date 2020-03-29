0f0186da405bef16797e1774d7998324479cfe98
Ricky-HPY <60127532+HPY-ricky@users.noreply.github.com>
2020-03-29 13:21:02 +0800

    api is work (done)

---
+++ FYP_WEB_APP/Models/API/SensorsAddDataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP_WEB_APP.Models.API
+{
+    public class SensorsAddDataModel
+    {
+        public string Sensorid{ get; set; }
+        public string Value{ get; set; }
+
+        internal void Equals()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
