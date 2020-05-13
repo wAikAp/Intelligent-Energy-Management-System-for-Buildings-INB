f533b0912b5026b88c71c7dd59a121224d1126cd
Ricky-HPY <60127532+HPY-ricky@users.noreply.github.com>
2020-05-14 02:37:43 +0800

    Update MongoDevicesListModel.cs

--- FYP_WEB_APP/Models/MongoModels/MongoDevicesListModel.cs
+++ FYP_WEB_APP/Models/MongoModels/MongoDevicesListModel.cs
@@ -16,7 +16,7 @@ namespace FYP_WEB_APP.Models.MongoModels
         public string devices_Name { get; set; }
         public int power { get; set; }
         public DateTime lastest_checking_time { get; set; }
-        public int total_run_time { get; set; }
+        public DateTime total_run_time { get; set; }
         public double pos_x { get; set; }
         public double pos_y { get; set; }
         public string desc { get; set; }
