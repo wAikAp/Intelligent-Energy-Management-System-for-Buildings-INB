8f409de43c279282c0cbad068e9cf67d18920b38
Ricky-HPY <60127532+HPY-ricky@users.noreply.github.com>
2020-05-24 04:38:38 +0800

    create weather

---
+++ FYP_WEB_APP/Controllers/API/WeatherDataAPIController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FYP_WEB_APP.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherDataAPIController : ControllerBase
+    {
+
+        // GET: api/<WeatherDataController>
+        [HttpGet]
+        public string Get()
+        {
+            return new WeatherDataController().GetWeather().ToJson();
+        }
+
+    }
+}
---
+++ FYP_WEB_APP/Controllers/WeatherDataController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using FYP_WEB_APP.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FYP_WEB_APP.Controllers
+{
+    public class WeatherDataController : Controller
+    {
+        private string dbName = "mydb";
+        private string CollectionName = "regional_weather";
+        private string ConnectionString = "mongodb+srv://hkteam1:IUXsr2ZYKQuPu0Sj@issf2020hk-la5xb.gcp.mongodb.net/test?retryWrites=true&w=majority";
+
+        // GET: api/<WeatherDataController>
+        [HttpGet]
+        public RegionalWeatherModel GetWeather()
+        {
+                     MongoClient dbClient = new MongoClient(ConnectionString);
+         IMongoDatabase database = dbClient.GetDatabase(dbName);
+
+        var sort = Builders<RegionalWeatherModel>.Sort.Descending("_id");
+            return database.GetCollection<RegionalWeatherModel>(CollectionName).Find<RegionalWeatherModel>(schedule => true).Sort(sort).FirstOrDefault(); ;
+
+        }
+        
+            public ActionResult Weather()
+        {
+
+            ViewData["RegionalWeatherModel"] = GetWeather();
+
+            return View("_Weather");
+        }
+    }
+
+}
