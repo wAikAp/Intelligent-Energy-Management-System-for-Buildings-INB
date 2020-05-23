adf9e49fd961188a987d6e9529aa176248477fc8
Ricky-HPY <60127532+HPY-ricky@users.noreply.github.com>
2020-05-24 02:20:14 +0800

    create api/status: get: set

---
+++ FYP_WEB_APP/Models/API/StatusModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP_WEB_APP.Models.API
+{
+    public class StatusModel
+    {
+        public string deviceId { get; set; }
+        public bool status { get; set; }
+        public double set_value { get; set; }
+        public DateTime lastest_checking_time { get; set; }
+    }
+}
