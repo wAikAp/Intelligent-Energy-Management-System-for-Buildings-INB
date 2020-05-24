8f839b42db43d0fda0c34275c2440d2359203166
WaiChan <william.sw.chan.0919@gmail.com>
2020-05-24 22:41:37 +0800

    cal heat index

--- FYP_WEB_APP/Controllers/WeatherController.cs
+++ FYP_WEB_APP/Controllers/WeatherController.cs
@@ -19,11 +19,12 @@ namespace FYP_WEB_APP.Controllers
         private string CollectionName = "regional_weather";
         RegionalWeatherModel model = new RegionalWeatherModel();
 
+        
         // GET: api/<WeatherDataController>
         [HttpGet]
         public RegionalWeatherModel GetWeather()
         {
-
+            this.calHeatIndex(27,80);
             try
             {
                 var sort = Builders<RegionalWeatherModel>.Sort.Descending("_id");
@@ -38,12 +39,37 @@ namespace FYP_WEB_APP.Controllers
         }
 
         [Route("Weather/Weather")]
-            public ActionResult Weather()
+        public ActionResult Weather()
         {
 
             ViewData["RegionalWeatherModel"] = GetWeather();
             return PartialView("Weather");
         }
+
+        public void calHeatIndex(double T, double RH) {
+            //heat index for out door indoor 
+            T = 1.8 * T + 32;
+            var HI = 0.5 * (T + 61 + (T - 68) * 1.2 + RH * 0.094);
+            if (HI >= 80)
+            {
+                HI = -42.379 + 2.04901523 * T + 10.14333127 * RH - .22475541 * T * RH
+                    - .00683783 * T * T - .05481717 * RH * RH + .00122874 * T * T * RH
+                    + .00085282 * T * RH * RH - .00000199 * T * T * RH * RH;
+                if (RH < 13 && 80 < T && T < 112)
+                {
+                    var ADJUSTMENT = (13 - RH) / 4 * Math.Sqrt((17 - Math.Abs(T - 95)) / 17);
+                    HI -= ADJUSTMENT;
+                }
+                else if (RH > 85 && 80 < T && T < 87) {
+                    var ADJUSTMENT = (RH - 85) * (87 - T) / 50;
+                    HI += ADJUSTMENT;
+                }
+            }
+            HI = Math.Round((HI - 32) / 1.8, 2);
+            Debug.WriteLine("HeatIndex = "+ HI);
+        }
+
+
     }
 
 }
