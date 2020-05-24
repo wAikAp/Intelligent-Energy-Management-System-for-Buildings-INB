2490bd5c2b24b87c07b04dc69c391ae074097b2b
WaiChan <william.sw.chan.0919@gmail.com>
2020-05-24 13:59:02 +0800

    intergrading weather api

--- FYP_WEB_APP/Controllers/WeatherController.cs
+++ FYP_WEB_APP/Controllers/WeatherController.cs
@@ -19,14 +19,21 @@ namespace FYP_WEB_APP.Controllers
         private string CollectionName = "regional_weather";
         private string ConnectionString = "mongodb+srv://hkteam1:IUXsr2ZYKQuPu0Sj@issf2020hk-la5xb.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
+
+
+
+
         // GET: api/<WeatherDataController>
         [HttpGet]
         public RegionalWeatherModel GetWeather()
         {
-                     MongoClient dbClient = new MongoClient(ConnectionString);
-         IMongoDatabase database = dbClient.GetDatabase(dbName);
 
-        var sort = Builders<RegionalWeatherModel>.Sort.Descending("_id");
+            MongoClient dbClient = new MongoClient(ConnectionString);
+
+            IMongoDatabase database = dbClient.GetDatabase(dbName);
+
+
+            var sort = Builders<RegionalWeatherModel>.Sort.Descending("_id");
             return database.GetCollection<RegionalWeatherModel>(CollectionName).Find<RegionalWeatherModel>(schedule => true).Sort(sort).FirstOrDefault(); ;
 
         }
